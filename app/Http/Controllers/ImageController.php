04c72abb52ec8c5bd78f78a986196478a51268f1
Syukri Mullia Adil Perkasa <syukri.mullia@gmail.com>
2018-02-25 22:24:50 +0700

    no need to save images to storage

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,40 +9,20 @@ class ImageController extends Controller
 {
     public function showKtpImage($id) {
         $familyRegistration = FamilyRegistration::find($id);
-
         $img = \Image::make(base64_decode($familyRegistration->ktp_photo));
-        $filename = $familyRegistration->id . '.png';
-
-        $storagePath = 'public/registration/' . $filename;
-        \Storage::put($storagePath, $img->encode('png', 100)->__toString());
-
         return $img->response();
 
     }
 
     public function showKkImage($id) {
         $familyRegistration = FamilyRegistration::find($id);
-
         $img = \Image::make(base64_decode($familyRegistration->kk_photo));
-        $filename = $familyRegistration->id . '.png';
-
-        $storagePath = 'public/registration/' . $filename;
-        \Storage::put($storagePath, $img->encode('png', 100)->__toString());
-
         return $img->response();
-
     }
 
     public function showBankAccountImage($id) {
         $bankAcc = BankAccount::find($id);
-
         $img = \Image::make(base64_decode($bankAcc->account_photo));
-        $filename = $bankAcc->id . '.png';
-
-        $storagePath = 'public/bank_account/' . $filename;
-        \Storage::put($storagePath, $img->encode('png', 100)->__toString());
-
         return $img->response();
-
     }
 }
